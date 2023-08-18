7bd654424a580f488b5ec509d0f85e5959ea0a21
Raffian-moin <raffianmoin69@gmail.com>
2023-08-18 13:23:48 +0600

    php object

---
+++ object.php
@@ -0,0 +1,38 @@
+<?php
+
+class Cake
+{
+    public $flour;
+    
+    public function makeCake($cake) 
+    {
+        echo "{$cake} cake will be ready in 30 minutes";
+    }
+}
+
+$vanilla = new Cake();
+$vanilla->makeCake('Vanilla');
+
+echo "\n";
+
+$apple = new Cake();
+$apple->makeCake('Apple');
+
+
+/*
+
+PHP classes can have objects.
+Object is an instance of a class. A class can have as many objects it needs.
+objects of a class can have different behaviours.
+
+1. object syntax:
+
+objectName = new className()
+
+2. accessing class method or property via object:
+
+$object->methodName()
+
+$object->propertyName
+
+*/
